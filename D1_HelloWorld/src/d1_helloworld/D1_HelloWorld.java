9339e4945918b8874a213915f71255a8b934c3ce
mdsiaofficial <mdsiaofficial@gmail.com>
2023-02-15 01:00:52 +0600

    aaa

--- D1_HelloWorld/src/d1_helloworld/D1_HelloWorld.java
+++ D1_HelloWorld/src/d1_helloworld/D1_HelloWorld.java
@@ -14,7 +14,7 @@ public class D1_HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
