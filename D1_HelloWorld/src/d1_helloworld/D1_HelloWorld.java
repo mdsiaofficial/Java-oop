9985389bfdfea8a2079e5d010147712d792f0e83
mdsiaofficial <mdsiaofficial@gmail.com>
2023-02-16 11:57:07 +0600

    aaa

---
+++ D1_HelloWorld/src/d1_helloworld/D1_HelloWorld.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package d1_helloworld;
+
+/**
+ *
+ * @author rvdas
+ */
+public class D1_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        int a=4565,b=34,c=33;
+        int sum=a+b+c;
+        int sub=a-b-c;
+        int mul=a*b*c;
+        int div=a/b/c;
+        System.out.println("Sum: "+ sum);
+        System.out.println("Sub: "+ sub);
+        System.out.println("Mul: "+ mul);
+        System.out.println("Div: "+ div);
+    }
+    
+}
