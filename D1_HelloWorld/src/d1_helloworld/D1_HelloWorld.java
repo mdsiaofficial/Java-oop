45cb6525cc82037afb0ea679e844616a72bb3d39
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-06 23:55:50 +0600

    ddd

--- D1_HelloWorld/src/d1_helloworld/D1_HelloWorld.java
+++ D1_HelloWorld/src/d1_helloworld/D1_HelloWorld.java
@@ -3,37 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package d1_helloworld;
-
 /**
  *
  * @author rvdas
  */
-
 public class D1_HelloWorld {
     /**
      * @param args the command line arguments
      */
     
-    static void hello(){
-        System.out.println("Hello world!");
-    }
-    static void hello(int a){
-        System.out.println("Hello world! " + a);
-    }
-    static void hello(double b){
-        System.out.println("Hello world! " + b);
-    }
-    static void hello(String name){
-        System.out.println("Hello " + name);
-    }
-    
-    int time=10;
     public static void main(String[] args) {
-        hello("ashiq");
-        D1_HelloWorld ghum = new D1_HelloWorld();
-        System.out.println(ghum.time);
-        System.out.println("Hello World");
-        hello();
+        
         int a=4565,b=34,c=33;
         int sum=a+b+c;
         int sub=a-b-c;
--- D2_Methods/src/d2_methods/D2_Methods.java
+++ D2_Methods/src/d2_methods/D2_Methods.java
@@ -13,7 +13,25 @@ public class D2_Methods {
     /**
      * @param args the command line arguments
      */
+    static void hello(){
+        System.out.println("Hello world!");
+    }
+    static void hello(int a){
+        System.out.println("Hello world! " + a);
+    }
+    static void hello(double b){
+        System.out.println("Hello world! " + b);
+    }
+    static void hello(String name){
+        System.out.println("Hello " + name);
+    }
+    int time=10;
     public static void main(String[] args) {
+        hello("ashiq");
+        D2_Methods ghum = new D2_Methods();
+        System.out.println(ghum.time);
+        System.out.println("Hello World");
+        hello();
         // TODO code application logic here
     }
     
