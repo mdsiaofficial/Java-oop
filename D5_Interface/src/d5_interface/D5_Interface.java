37b55c97bda7043b3e14a4d5e095e724c97bc91d
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-17 12:23:30 +0600

    interface

--- D5_Interface/src/d5_interface/D5_Interface.java
+++ D5_Interface/src/d5_interface/D5_Interface.java
@@ -2,24 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package d5_abstraction;
+package d5_interface;
 
 /**
  *
  * @author rvdas
  */
-public class D5_Abstraction {
+public class D5_Interface {
 
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         // TODO code application logic here
-        MobileUser mu_a = new Ashiq();
-        MobileUser mu_m = new Mahida();
-        mu_a.SendMessage();
-        mu_m.SendMessage();
     }
     
 }
