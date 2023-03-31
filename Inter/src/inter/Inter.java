808d2f1ca5efa57e01aff968b3913d94976cf116
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-31 08:15:38 +0600

    new

--- Inter/src/inter/Inter.java
+++ Inter/src/inter/Inter.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package inter;
 interface Area {
     void what();
@@ -23,7 +19,6 @@ class Warehouse {
 }
 public class Inter {
     public static void main(String[] args) {
-        // TODO code application logic here
         Market s = new Market();
         House h = new House();
         Warehouse w = new Warehouse();
