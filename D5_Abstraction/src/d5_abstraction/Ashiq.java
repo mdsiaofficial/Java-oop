b5469083df724450413f5e9d627530741307dd94
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-31 07:46:00 +0600

    new

--- D5_Abstraction/src/d5_abstraction/Ashiq.java
+++ D5_Abstraction/src/d5_abstraction/Ashiq.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package d5_abstraction;
 
-/**
- *
- * @author rvdas
- */
 public class Ashiq extends MobileUser {
     @Override
     void SendMessage(){
