4a3e551fba68575b42843ebf4252808ea1e9dedf
mdsiaofficial <mdsiaofficial@gmail.com>
2023-05-17 21:49:06 +0600

    new

---
+++ Final/D4_Multi_Thread/src/Sync/Sync_Block.java
@@ -0,0 +1,49 @@
+package Sync;
+
+class Table {
+    void PrintTable(int n){
+        for(int i=0; i<=5; i++){
+            System.out.println(n*i+" ");
+            try{
+                Thread.sleep(400);
+                //sleep(400);
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class NewThread_1 extends Thread{
+    Table t;
+    NewThread_1(Table t){
+        this.t = t;
+    }
+    public void run(){
+        synchronized (t){
+            t.PrintTable(5);
+        }
+    }
+}
+
+class NewThread_2 extends Thread{
+    Table t;
+    NewThread_2(Table t){
+        this.t = t;
+    }
+    public void run(){
+        synchronized (t){
+            t.PrintTable(100);
+        }
+    }
+}
+
+public class Sync_Block {
+    public static void main(String[] args) {
+        Table obj = new Table();
+        NewThread_1 t1 = new NewThread_1(obj);
+        NewThread_2 t2 = new NewThread_2(obj);
+        t1.start();
+        t2.start();
+    }
+}
