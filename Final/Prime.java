572e96d9a1a2cba475cb2821411d57e1294cca3b
mdsiaofficial <mdsiaofficial@gmail.com>
2023-06-01 23:50:50 +0600

    new

--- Final/Prime.java
+++ Final/Prime.java
@@ -1,33 +1,21 @@
-public class Main {
-
+import java.util.*;
+class Prime {
 	public static boolean isPrime(int n) {	  
-
 	  if (n < 2) {
-
 	    return false;
-
 	  }
-
 	  for (int i = 2; i * i <= n; i++) {
-
 	    if (n % i == 0) {
-
 	      return false;
-
 	    }
-
 	  }
-
 	  return true;
-
 	}
-
-	
-
 	public static void main(String[] args) {
-
-		System.out.println(isPrime(11));
-
+		int n;
+		Scanner in = new Scanner(System.in);
+		n=in.nextInt();
+		System.out.println(isPrime(n));
 	}
 
 }
